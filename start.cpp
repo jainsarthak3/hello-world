db130e61e444dc4632be44ad737aa72bb141e5ed
Sarthak Jain <jainsarthak3@gmail.com>
2016-06-28 12:43:41 +0530

    Create start.cpp
    A rightful declaration to my start

---
+++ start.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout<<"MY first github repository"<<endl;
+	cout<<"Hello-World"<<endl;
+	cout<<"Now Watch out for me as I commence my journey"<<endl;
+	return 0;
+}
