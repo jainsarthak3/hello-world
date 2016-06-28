7fbe123623fe565a60e919d6647d753fe73e3d9d
Sarthak Jain <jainsarthak3@gmail.com>
2016-06-28 12:44:43 +0530

    Update start.cpp

--- start.cpp
+++ start.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 	cout<<"MY first github repository"<<endl;
-	cout<<"Hello-World"<<endl;
-	cout<<"Now Watch out for me as I commence my journey"<<endl;
+	cout<<"Hello-World"<<endl; 
+	cout<<"Now Watch out for me as I commence my journey"<<endl; // first changes
 	return 0;
 }
